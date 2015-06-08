883c4a701052eede03eb64802dfee718398d4810
vagrant <vagrant@localhost.localdomain>
2015-06-08 15:46:45 +0000

    proceeded 01_javabook

--- Cleric.java
+++ Cleric.java
@@ -2,9 +2,23 @@ import java.util.*;
 public class Cleric {
     String name;
     int hp = 50;
-    final int MAX_HP = 50;
+    static final int MAX_HP = 50;
     int mp = 10;
-    final int MAX_MP = 10;
+    static final int MAX_MP = 10;
+
+    public Cleric(String name, int hp, int mp) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+    }
+
+    public Cleric(String name, int hp) {
+        this(name, hp, Cleric.MAX_MP);
+    } 
+
+    public Cleric(String name) {
+        this(name, Cleric.MAX_HP);
+    }
 
     public void selfAid() {
         System.out.println(this.name + "はセルフエイドを唱えた！");
--- Hero.java
+++ Hero.java
@@ -1,31 +1,79 @@
 public class Hero {
-    String name; // 名前の宣言
-    int hp; // HPの宣言
-    Sword sword;
-
-    void attack() {
-
+    private String name; 
+    private int hp; 
+    private static int money;
+    private Sword sword;
+    
+    public String getName() {
+        return this.name;
     }
 
-    void run() {
-        System.out.println(this.name + "は、逃げだした！");
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("名前がnullです。処理を中断します。");
+        }
+
+        if (name.length() <= 1) {
+            throw new IllegalArgumentException("名前が短すぎます。処理を中断します。");
+        }
+
+        if (name.length() >= 8) {
+            throw new IllegalArgumentException("名前が長すぎます。処理を中断します。");
+        }
+        
+        this.name = name;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public Sword getSword() {
+        return this.sword;
+    }
+
+    public void setSword(Sword sword) {
+        this.sword = sword;
+    }
+    
+    public void bye() {
+        System.out.println("勇者は別れを告げた");
+    }
+
+    private void die() {
+        System.out.println(this.name + "は死んでしまった！");
         System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした");
     }
 
-    void sit(int sec) {
+    // 戦う
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        m.hp -= 5;
+        System.out.println("5ポイントのダメージを与えた！");
+    }
+
+    // 逃げる
+    public void run() {
+        System.out.println(this.name + "は、逃げだした！");
+    }
+
+    public void sit(int sec) {
         this.hp += sec;
         System.out.println(this.name + "は、" + sec + "秒座った！");
         System.out.println("HPが" + sec + "ポイント回復した");
     }
 
-    void slip() {
+    public void slip() {
         this.hp -= 5;
         System.out.println(this.name + "は、転んだ！");
         System.out.println("5のダメージ！");
     }
 
-    void sleep() {
+    public void sleep() {
         this.hp = 100;
         System.out.println(this.name + "は、眠って回復した！" );
     }
@@ -38,6 +86,10 @@ public class Hero {
     Hero() {
         this.hp = 100;
         this.name = "ダミー";
+    }
+
+    public static void setRandomMoney() {
+        Hero.money = (int) (Math.random() * 1000);
     }
 
 }
